f90f43f4c8b145bb749963702adfff9364013298
Dung Huynh <htd1999@gmail.com>
2020-06-30 18:58:47 +0700

    update sql

--- src/main/java/com/example/demo/controller/ChapterController.java
+++ src/main/java/com/example/demo/controller/ChapterController.java
@@ -25,7 +25,7 @@ public class ChapterController {
      * Lấy các images của chapter đó,dựa vào chapterID
      */
     public Object getChapterImagesByChapterID(@PathVariable("chapterID") String chapterID) {
-        String chapterID2 = chapterID.trim();
+        /*String chapterID2 = chapterID.trim();
         if (chapterID2 == null || chapterID2.isEmpty())
             return new ResponseEntity("Chapter id can't be empty",
                     HttpStatus.NOT_ACCEPTABLE);
@@ -38,7 +38,26 @@ public class ChapterController {
 
         } catch (Exception e) {
         }
-        return new ResponseEntity("Exception occured", HttpStatus.NOT_FOUND);
+        return new ResponseEntity("Exception occured", HttpStatus.NOT_FOUND);*/
+        List<String> returnData = new ArrayList<>();
+        returnData.add("https://i2.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132275.jpg");
+        returnData.add("https://i2.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132277.jpg");
+        returnData.add("https://i6.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132279.jpg");
+        returnData.add("https://i6.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132281.jpg");
+        returnData.add("https://i8.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132283.jpg");
+        returnData.add("https://i6.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132285.jpg");
+        returnData.add("https://i4.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132287.jpg");
+        returnData.add("https://i10.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132289.jpg");
+        returnData.add("https://i6.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132291.jpg");
+        returnData.add("https://i4.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132293.jpg");
+        returnData.add("https://i5.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132259.jpg");
+        returnData.add("https://i2.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132295.jpg");
+        returnData.add("https://i4.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132297.jpg");
+        returnData.add("https://i10.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132299.jpg");
+        returnData.add("https://i2.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132301.jpg");
+        returnData.add("https://i4.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132303.jpg");
+        returnData.add("https://i6.mangapanda.com/valkyria-chronicles/10/valkyria-chronicles-4132305.jpg");
+        return returnData;
     }
 
     @GetMapping("/{bookID}")
@@ -48,6 +67,9 @@ public class ChapterController {
      */
     public Object getChaptersByBookID(@PathVariable("bookID") String bookID) {
         List<ChapterEntity> tempData = service.getChaptersByBookID(bookID);
+        if(tempData == null){
+            return new ResponseEntity(null, HttpStatus.OK);
+        }
         List<ReturnChapterEntity> returnData = new ArrayList<ReturnChapterEntity>(tempData.size());
         tempData.stream().forEach(x -> {
             returnData.add(new ReturnChapterEntity(x.getId(), x.getChapterName(), x.getDate()));
